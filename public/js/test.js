460ced81be21232dbd37fd91f504d8456a3445a9
xiaoshuoyao <x5yao@ucsd.edu>
2021-02-23 02:37:38 +0800

    created json to temporally store word

--- public/js/test.js
+++ public/js/test.js
@@ -34,3 +34,21 @@ function GroupClick(e) {
     window.location.href='GPH';
 }
 
+function searchWord(e) {
+    word = req.query.word;
+    const settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://twinword-word-graph-dictionary.p.rapidapi.com/examhistory/?entry=mask",
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "1db4c8f7e1msh630a5ef79301c90p19a55bjsn4e6d426ef3c4",
+        "x-rapidapi-host": "twinword-word-graph-dictionary.p.rapidapi.com"
+      }
+    };
+    
+    $.ajax(settings).done(function(response) {
+      console.log(response);
+    });
+}
+
--- routes/search.js
+++ routes/search.js
@@ -1,18 +1,3 @@
 exports.view = function(req, res){
-    word = req.query.word;
-    const settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://twinword-word-graph-dictionary.p.rapidapi.com/examhistory/?entry=mask",
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-key": "1db4c8f7e1msh630a5ef79301c90p19a55bjsn4e6d426ef3c4",
-        "x-rapidapi-host": "twinword-word-graph-dictionary.p.rapidapi.com"
-      }
-    };
-    
-    $.ajax(settings).done(function(response) {
-      console.log(response);
-    });
-    
-  };
+    res.render('search');
+};
